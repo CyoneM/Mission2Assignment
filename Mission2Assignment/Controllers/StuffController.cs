f40bef6dcbf617c6cb68329b14605e9a40f5ff8c
CyoneM <mtdavis32@gmail.com>
2022-01-19 22:18:49 -0700

    Switching to MVC

---
+++ Mission2Assignment/Controllers/StuffController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment.Controllers
+{
+    public class StuffController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new IndexOutOfRangeException();
+            // return View();
+        }
+    }
+}
