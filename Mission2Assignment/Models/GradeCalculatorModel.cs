da7d26565d7a5f2a3d0d7bc110ca3c3807fc9f8a
CyoneM <mtdavis32@gmail.com>
2022-02-19 15:30:48 -0800

    Fix post attempt

---
+++ Mission2Assignment/Models/GradeCalculatorModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Required]
+        [Range(0, 100)]
+        public float Assignment { get; set; }
+        [Range(0, 100)]
+        public float GroupProject { get; set; }
+        [Range(0, 100)]
+        public float Quiz { get; set; }
+        [Range(0, 100)]
+        public float Exam { get; set; }
+        [Range(0, 100)]
+        [MaxLength(5)]
+        public float Intex { get; set; }
+    }
+}
